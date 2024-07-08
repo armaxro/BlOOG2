7846878ce5fa11e1b1ceb6732b26101b1143660c
Maxi Rojas <arnaldomrojas@outlook.com>
2024-07-08 14:37:54 +0200

    permission

---
+++ test/Maxi/07-permission/model/Mapping/Permission.php
@@ -0,0 +1,20 @@
+<?php
+class Permission {
+
+/*
+Proprietees -> equivalent de variable
+*/
+private ?int $permission_id = null;
+private ?string $permission_name = null;
+private ?string $permission_description = null;
+
+/*
+Constante
+*/
+
+/*
+Methode
+*/
+
+
+};
---
+++ test/Maxi/07-permission/public/index.php
@@ -0,0 +1,16 @@
+<?php
+//creation de l'instance
+require_once "../config.php";
+//connexion à la db
+//manuellement nous importons notre classe de mapping
+require "../model/Mapping/Permission.php";
+
+$db = new PDO(DB_TYPE.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET,
+DB_LOGIN,
+DB_PWD);
+//tableau associatif en fetch
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$perm1 = new Permission();
+
+var_dump($perm1);
